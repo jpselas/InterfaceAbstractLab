75b5e99b28a17ad0b943196186d0cc9a56bbc4d0
johnny <johnny@172.16.100.13>
2015-09-15 15:41:41 -0500

    InterfaceAbstract LAB StartUp class created

---
+++ src/lab1/StartUp.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+/**
+ *
+ * @author johnny
+ */
+public class StartUp {
+    
+    public static void main(String[] args) {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+}
