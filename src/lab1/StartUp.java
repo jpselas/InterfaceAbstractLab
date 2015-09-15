1116fda23e1e9ad9509df71ae3a958c5e43f934b
johnny <johnny@172.16.100.13>
2015-09-15 15:53:09 -0500

    InterfaceAbstract LAB StartUp class finished

--- src/lab1/StartUp.java
+++ src/lab1/StartUp.java
@@ -12,9 +12,21 @@ package lab1;
 public class StartUp {
     
     public static void main(String[] args) {
-    
-    
-    
+        
+        // Advantagoeus for when you want to create an array of different objects also needed for polymorphism
+        // Disadvantage if you are not using abstract class or interface 
+        
+        
+        Course first = new AdvancedJavaCourse("Java: Advanced Programming","AJ234");
+        
+        Course second = new IntroJavaCourse("Java 101","I455");
+        
+        Course third = new IntroToProgrammingCourse("Programming for Dummies","PD567");
+        
+        System.out.println("Here is your course list for the semester");
+        System.out.println("CLass 1: " + first.getCourseName() + " and it is course number: " + first.getCourseNumber());
+        System.out.println("CLass 2: " + second.getCourseName() + " and it is course number: " + second.getCourseNumber());
+        System.out.println("CLass 3: " + third.getCourseName() + " and it is course number: " + third.getCourseNumber());
     
     
     
